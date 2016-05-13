044211858f817083218df5334138c4f5f31b7f0e
Masaru Shimizu <shimizu@sist.chukyo-u.ac.jp>
2016-05-13 09:14:24 +0900

    Fix_pioneer3at

--- USARGazebo.cc
+++ USARGazebo.cc
@@ -1739,6 +1739,59 @@ void USARcommand::UC_check_command_from_USARclient(void)
 //  Image Server
 //#######################################################################
 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <jpeglib.h>
+int main () {
+    unsigned char *outbuffer_pt = NULL;
+    unsigned long int outlen = 0;
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+    jpeg_mem_dest(&cinfo, &outbuffer_pt, &outlen);
+//  jpeg_stdio_dest(&cinfo, fp);
+    int width = 256;
+    int height = 256;
+    cinfo.image_width = width;
+    cinfo.image_height = height;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_RGB;
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, 75, TRUE);
+    jpeg_start_compress(&cinfo, TRUE);
+    JSAMPARRAY img = (JSAMPARRAY) malloc(sizeof(JSAMPROW) * height);
+    for (int i = 0; i < height; i++) {
+        img[i] = (JSAMPROW) malloc(sizeof(JSAMPLE) * 3 * width);
+        for (int j = 0; j < width; j++) {
+            img[i][j*3 + 0] = i;
+            img[i][j*3 + 1] = j;
+            img[i][j*3 + 2] = 127;
+        }
+    }
+    jpeg_write_scanlines(&cinfo, img, height);
+    jpeg_finish_compress(&cinfo);
+    jpeg_destroy_compress(&cinfo);
+    for (int i = 0; i < height; i++) {
+        free(img[i]);
+    }
+    free(img);
+    const char *filename = "output.jpg";
+    FILE *fp = fopen(filename, "wb");
+    if (fp == NULL) {
+        fprintf(stderr, "cannot open %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+   fwrite((const void*)outbuffer_pt, outlen, 1, fp);
+   fclose(fp);
+   if(NULL != outbuffer_pt)
+   {
+     free(outbuffer_pt);
+   }
+}
+*/
+
 //////////////////////////////////////////////////////////////////
 // USARimage : A structure for transferring image data
 struct USARimage
